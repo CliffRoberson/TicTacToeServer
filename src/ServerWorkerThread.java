c2681d3e8a9b814cc7641f001746cac254d63468
clifford <clifford.k.roberson@vanderbilt.edu>
2014-03-24 14:48:53 -0500

    Added debugging message to server to test connectivity

--- src/ServerWorkerThread.java
+++ src/ServerWorkerThread.java
@@ -27,10 +27,10 @@ public class ServerWorkerThread extends Thread {
 	@Override
 	public void run() {
 		
-		try{
+		
 			String payload = new String(rxPacket.getData(), 0, rxPacket.getLength()).trim();
 
-			//System.out.println(payload);
+			System.out.println("Recieved " + payload + " from " + rxPacket.getAddress().toString());
 			
 			if (payload.startsWith("REGISTER")) {
 				onRegisterRequested(payload);
@@ -51,39 +51,14 @@ public class ServerWorkerThread extends Thread {
 			}else if (payload.startsWith("QUIT")){
 				onQuitRequested(payload);
 				return;
+			}else {
+				
 			}
-			
-			
-			/*
-			else if (payload.startsWith("GETGROUPS")) {
-				onGetGroupsRequested();
-				return;
-			} else if (payload.startsWith("QUIT")){
-				onUnregisterRequested(payload);
-			}else if (payload.startsWith("SETGROUPS")){
-				payload = payload.substring("SETGROUPS".length() + 1,
-		                payload.length()).trim();
-				onSetGroupsRequested(payload);
-			}else if (payload.startsWith("ACK")){
-				onClientAck();
-			}else if(payload.startsWith("SENDMETHESTUFF")){
-				onSendSTUFF();
-			}else if(payload.startsWith("CREATEGROUP")){
-				payload = payload.substring("CREATEGROUP".length() + 1,
-		                payload.length()).trim();
-				onCreateGroup(payload);
-			}*/else {
-				//onChatReceived(payload);
-			}
-		}finally{
-			
-		}
-		
 			
 			
 	}
 
-	//Reads in name followed by what groups name belongs to
+	
 		private void onRegisterRequested(String payload) {
 
 			Random rand = new Random();
@@ -155,115 +130,6 @@ public class ServerWorkerThread extends Thread {
 		}
 	}
 	
-	/*
-	private void onGetGroupsRequested() throws IOException{
-		String aString = "GETGROUPS ";
-		for(String s : Server.allGroups){
-			aString = aString + s + " ";
-		}
-		send(aString,address,port);	
-			
-		
-	}
-	
-	private void onCreateGroup(String payload) throws IOException{
-		dataScanner = new Scanner(payload);
-		while (dataScanner.hasNext()){
-			 Server.allGroups.add(dataScanner.next());
-		 }
-		onGetGroupsRequested();
-		
-	}
-	
-	
-	
-	private void onSendSTUFF() throws IOException{
-		for (ClientEndPoint clientEndPoint : Server.clientEndPoints) {
-			
-			 if (clientEndPoint.address.equals(address) && clientEndPoint.port == port){
-				 if (!clientEndPoint.messages.isEmpty()){
-					 if (clientEndPoint.numberOfTimesMessageSent > 20000000){
-						 clientEndPoint.messages.remove();
-						 clientEndPoint.numberOfTimesMessageSent = 0;
-					 }else if (clientEndPoint.messages.size() > 1){
-						 
-						 send(clientEndPoint.messages.peek(), address, port);
-						 clientEndPoint.numberOfTimesMessageSent++;
-						 onSendSTUFF();
-						 return;
-					 }
-					 //System.out.println("here");
-					 
-					 try {
-						send(clientEndPoint.messages.peek(), address, port);
-						clientEndPoint.numberOfTimesMessageSent++;
-					} catch (IOException e) {
-						
-						e.printStackTrace();
-					}
-				 }
-			 }
-		}
-		
-		
-	}
-	
-	private void onClientAck(){
-		Server.clientEndPoints.get(key)
-		
-		for (ClientEndPoint clientEndPoint : Server.clientEndPoints) {
-			 if (clientEndPoint.address.equals(address) && clientEndPoint.port == port){
-				 clientEndPoint.numberOfTimesMessageSent = 0;
-				
-				 clientEndPoint.messages.poll();
-			 }
-		}
-	}
-	
-	private void onSetGroupsRequested(String payload){
-		Set<String> groups = Collections.synchronizedSet(new HashSet<String>());
-		dataScanner = new Scanner(payload);
-		
-		
-			for (ClientEndPoint clientEndPoint : Server.clientEndPoints) {
-				 if (clientEndPoint.address.equals(address) && clientEndPoint.port == port){
-					 String someGroups = "";
-					 clientEndPoint.groups.clear();
-					 while (dataScanner.hasNext()){
-						 String aGroup = dataScanner.next();
-						 groups.add(aGroup);
-						 someGroups = someGroups + aGroup + " ";
-					 }
-					 
-					 
-					 clientEndPoint.groups = groups;
-					 clientEndPoint.messages.add("Groups set to " + someGroups);
-				 }
-			}
-			
-			
-		
-	}
-	
-	private void onUnregisterRequested(String payload) {
-        
-		
-		 for (ClientEndPoint clientEndPoint : Server.clientEndPoints) {
-			 if (clientEndPoint.address.equals(address) && clientEndPoint.port == port){
-				 Server.clientEndPoints.remove(clientEndPoint);
-				 
-				 return;
-			 }       
-		 } 
-		 
-		 for (ClientEndPoint clientEndPoint : Server.clientEndPoints) {
-			 if (clientEndPoint.address.equals(address) && clientEndPoint.port == port){
-				 clientEndPoint.messages.add("NOT REGISTERED, so nothing to unregister");
-			 }
-        
-		 }
-	}
-		 */
 
 
 	// send a string, wrapped in a UDP packet, to the specified remote endpoint
@@ -281,34 +147,6 @@ public class ServerWorkerThread extends Thread {
 	
 	
 
-/*
-	private void onChatReceived(String payload) {
-		// get the address of the sender from the rxPacket
-				InetAddress address = this.rxPacket.getAddress();
-				// get the port of the sender from the rxPacket
-				int port = this.rxPacket.getPort();
-				Set<String> groups = Collections.synchronizedSet(new HashSet<String>());
-				String name = "foobar";
-		
-	        for (ClientEndPoint clientEndPoint : Server.clientEndPoints) {
-	        	
-	        		if (clientEndPoint.address.equals(address) && clientEndPoint.port == port){
-	        			groups = clientEndPoint.groups;
-	        			name = clientEndPoint.name;
-	        		}
-	        }
-	        if (groups != null){
-			    for (String groupName : groups){
-			        for (ClientEndPoint clientEndPoint : Server.clientEndPoints) {
-			        	if (clientEndPoint.groups.contains(groupName)){
-			        		
-			        		clientEndPoint.messages.add(name + ": " + payload);
-			        	}
-			        }
-			    }   
-	        }   
-        
-        }*/
 	}
 
 
